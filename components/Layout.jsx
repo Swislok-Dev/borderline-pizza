88416704c970858712b40c3f707cab0f8d6c1ae8
Swislok-Dev <cmarker4@gmail.com>
2022-10-27 08:30:31 -0500

    adjust padding

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -15,7 +15,7 @@ export default function Layout({ title, children }) {
 
       <div className="flex min-h-screen flex-col justify-between">
         <header>
-          <nav className="flex h-12 items-center px-4 justify-between shadow">
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/">
               <a className="text-lg font-bold">Borderline Pizza</a>
             </Link>
@@ -27,7 +27,7 @@ export default function Layout({ title, children }) {
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center">
+        <footer className="flex h-10 justify-center items-center shadow-inner">
           <p>Copyright © 2022 Borderline Pizza</p>
         </footer>
       </div>
