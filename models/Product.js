94c06b8de30b75f5dd3156d55212553022d39711
cm@MBA <cmarker4@gmail.com>
2023-01-05 12:09:38 -0600

    update product model

--- models/Product.js
+++ models/Product.js
@@ -3,10 +3,11 @@ import { mongoose } from "mongoose";
 const productSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    prices: { type: Object, required: true },
+    prices: { type: Object, required: false },
     slug: { type: String, required: true, unique: true },
     category: { type: String, required: true },
     options: { type: Object, required: false },
+    description: {type: String, required: false}
   },
   {
     timestamps: true,
