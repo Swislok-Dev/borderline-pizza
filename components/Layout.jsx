5d9683b89ca79b07912c937bfc891bdb009244c6
cm@MBA <cmarker4@gmail.com>
2023-01-05 12:33:47 -0600

    add back  react-icon button

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { BsChevronLeft } from "react-icons/bs";
 
 export default function Layout({ title, children }) {
+  const [isBackButtonVisible, setIsBackButtonVisible] = useState("hidden");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname !== "/" && router.pathname !== "/menu") {
+      setIsBackButtonVisible("visible");
+    }
+  }, [router]);
+
   return (
     <>
       <Head>
@@ -16,14 +27,23 @@ export default function Layout({ title, children }) {
       <div className="flex min-h-screen flex-col justify-between">
         <header className="sticky top-0 z-10 w-full bg-white shadow">
           <nav className="z-1 flex h-12 items-center justify-between px-4 ">
+            <BsChevronLeft
+              style={{
+                visibility: isBackButtonVisible,
+                fontSize: "24px",
+                marginRight: "24px",
+                cursor: "pointer",
+              }}
+              onClick={() => router.back()}
+            />
+
             <Link href="/">
               <a className="text-lg font-bold">Borderline Pizza</a>
             </Link>
-            <div>
-              <Link href="/menu">
-                <a className="p-2">Menu</a>
-              </Link>
-            </div>
+
+            <Link href="/menu">
+              <a className="">Menu</a>
+            </Link>
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4 text-center">
