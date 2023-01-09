dec56e5cbc5778d5c01cc923ed0e57d3241b2c4e
cm@MBA <cmarker4@gmail.com>
2023-01-09 11:18:37 -0600

    change to .jsx file

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout.jsx";
-
-export default function Home() {
-  return (
-    <Layout>
-      <h1 className="text-3xl">Borderline Pizza</h1>
-    </Layout>
-  );
-}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout.jsx";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div id="hero">
+        <h1 className="text-3xl">Borderline Pizza & Taco Jerry&apos;s</h1>
+        <p>Established - 1981</p>
+        <p>&quot;Best Pizza Anywhere&quot;</p>
+      </div>
+    </Layout>
+  );
+}
