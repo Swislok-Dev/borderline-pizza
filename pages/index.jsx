7a5aa15e0abf9bba942bffebf50f79b00a302f00
cm@MBA <cmarker4@gmail.com>
2023-02-01 13:10:53 -0600

    add more store info

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,14 @@ export default function Home() {
     <Layout>
       <div id="hero">
         <h1 className="text-3xl">Borderline Pizza & Taco Jerry&apos;s</h1>
-        <p>Established - 1981</p>
-        <p>&quot;Best Pizza Anywhere&quot;</p>
+        <section id="store-info">
+          <p>&quot;Best Pizza Anywhere&quot;</p>
+          <div id="store-hours">
+            <p>Monday - Saturday</p>
+            <p>11AM - 9PM</p>
+          </div>
+          <p>Closed on Sunday</p>
+        </section>
       </div>
     </Layout>
   );
