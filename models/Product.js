bcaca3e2d961dd140c0aa398ec7c5be89e0d3801
cm@MBA <cmarker4@gmail.com>
2023-02-07 08:53:17 -0600

    update title to except empty string

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ import { mongoose } from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
+    title: { type: String, required: false },
     prices: { type: Object, required: false },
     slug: { type: String, required: true, unique: true },
     category: { type: String, required: true },
