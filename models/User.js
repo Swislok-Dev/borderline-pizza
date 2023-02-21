f74ebeed646f3b6fe7552b0ca84a37d49277e66a
cm@MBA <cmarker4@gmail.com>
2023-02-21 12:05:00 -0600

    add User model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    isAdmin: { type: String, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
