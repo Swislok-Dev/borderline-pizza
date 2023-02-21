b4b8717451b2ce942f51d4a9f43b73a2175150f5
cm@MBA <cmarker4@gmail.com>
2023-02-21 12:00:44 -0600

    add login page

---
+++ pages/admin/login.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import Layout from "../../components/Layout";
+
+function login() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  } = useForm();
+
+  const submitHandler = ({ email, password }) => {
+    console.log("email :>> ", email);
+    console.log("password :>> ", password);
+  };
+  return (
+    <Layout title="Login">
+        <h2>Login</h2>
+      <form onSubmit={handleSubmit(submitHandler)} className="border">
+        <div className="form-inputs">
+          <label htmlFor="email">Email</label>
+          <input
+            {...register("email", {
+              required: "Please enter email",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+.[a-zA-Z0-9-.]+$/i,
+                message: "Please enter email",
+              },
+            })}
+            type="email"
+            id="email"
+            autoFocus
+          />
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="form-inputs">
+          <label htmlFor="password">Password</label>
+          <input type="password"
+          {...register('password', {
+            required: 'Please enter password',
+            minLength: { value: 6, message: 'password must be longer than 5 characters'}
+          })}
+          id="password"
+          autoFocus
+           />
+           {errors.password && (
+            <div className="text-red-500">{errors.password.message}</div>
+           )}
+        </div>
+        <div className="form-inputs">
+          <button>Login</button>
+        </div>
+        
+      </form>
+    </Layout>
+  );
+}
+
+export default login;
