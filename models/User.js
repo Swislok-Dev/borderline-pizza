b7059e9d889d04e93a4fd4e4ff4741b4fe39c2c9
cm@MBA <cmarker4@gmail.com>
2023-02-23 10:27:50 -0600

    add isDev property

--- models/User.js
+++ models/User.js
@@ -6,6 +6,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
     isAdmin: { type: String, required: true, default: false },
+    isDev: { type: String, required: true, default: false }
+
   },
   {
     timestamps: true,
