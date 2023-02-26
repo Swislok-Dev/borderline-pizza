4ad3514fb07e618eb3e427433738be6ce8993f9d
cm@MBA <cmarker4@gmail.com>
2023-02-26 14:06:12 -0600

    seed only data object

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,21 +1,20 @@
 import db from "../../utils/db";
 import Product from "../../models/Product";
 import data from "../../utils/data";
-import Categories from '../../models/Categories';
-import User from "../../models/User";
-import users from '../../utils/users';
+import Categories from "../../models/Categories";
 
 const handler = async (req, res) => {
   await db.connect();
   await Product.deleteMany();
   await Product.insertMany(data.products);
   await Categories.deleteMany();
-  await Categories.insertMany(data.categories)
-  await User.deleteMany();
-  await User.insertMany(users.users)
+  await Categories.insertMany(data.categories);
+
   await db.disconnect();
-  await res.send({ message: "Products and Categories seeded successfuly", data });
-  
+  await res.send({
+    message: "Products and Categories seeded successfuly",
+    data,
+  });
 };
 
 export default handler;
