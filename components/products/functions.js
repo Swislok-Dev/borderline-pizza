2cccf4b575f11038f567413af76a509b5d524d7c
cm@MBA <cmarker4@gmail.com>
2023-02-26 14:12:06 -0600

    comment import

--- components/products/functions.js
+++ components/products/functions.js
@@ -1,4 +1,4 @@
-import { itemDescription } from "../../utils/data";
+// import { itemDescription } from "../../utils/data";
 
 export const isObject = function (val) {
   if (val === null) {
@@ -121,70 +121,71 @@ export const showItemDescription = (category, description) => {
 };
 
 export const showCategoryDescription = ({ category }) => {
-  if (itemDescription.includes(category)) {
-    switch (category) {
-      case "pizza":
-        return (
-          <>
-            <section className="additional-fields">
-              <h3 className="additional-fields-heading">Available Toppings</h3>
-              <div className="grid grid-cols-2">
-                {/* {pizzaToppings.map((el) => {
-                  return (
-                    <p className="font-semibold" key={el}>
-                      {el}
-                    </p>
-                  );
-                })} */}
-              </div>
-            </section>
+  console.log(category)
+  // if (itemDescription.includes(category)) {
+  //   switch (category) {
+  //     case "pizza":
+  //       return (
+  //         <>
+  //           <section className="additional-fields">
+  //             <h3 className="additional-fields-heading">Available Toppings</h3>
+  //             <div className="grid grid-cols-2">
+  //               {/* {pizzaToppings.map((el) => {
+  //                 return (
+  //                   <p className="font-semibold" key={el}>
+  //                     {el}
+  //                   </p>
+  //                 );
+  //               })} */}
+  //             </div>
+  //           </section>
 
-            <p className="m-2">
-              *These toppings are not included automatically but are available
-              by request at no additional charge.
-            </p>
-            <br />
-            <p className="font-medium">
-              Like Supreme Pizza? Try our &quot;Borderline 9&quot;
-            </p>
-          </>
-        );
+  //           <p className="m-2">
+  //             *These toppings are not included automatically but are available
+  //             by request at no additional charge.
+  //           </p>
+  //           <br />
+  //           <p className="font-medium">
+  //             Like Supreme Pizza? Try our &quot;Borderline 9&quot;
+  //           </p>
+  //         </>
+  //       );
 
-      case "specialty pizza":
-        return (
-          <div className="m-2">
-            <section className="additional-fields">
-              <h3 className="additional-fields-heading">Available Choices</h3>
+  //     case "specialty pizza":
+  //       return (
+  //         <div className="m-2">
+  //           <section className="additional-fields">
+  //             <h3 className="additional-fields-heading">Available Choices</h3>
 
-              <div className="grid grid-cols-2">
-                {/* {specialtyPizzas.map((el) => {
-                  return (
-                    <p className="font-semibold" key={el}>
-                      {el}
-                    </p>
-                  );
-                })} */}
-              </div>
-            </section>
+  //             <div className="grid grid-cols-2">
+  //               {/* {specialtyPizzas.map((el) => {
+  //                 return (
+  //                   <p className="font-semibold" key={el}>
+  //                     {el}
+  //                   </p>
+  //                 );
+  //               })} */}
+  //             </div>
+  //           </section>
 
-            <p>
-              *includes canadian bacon, pepperoni, hamburger, sausage and bacon
-            </p>
-            <p>**includes onion and green pepper</p>
-          </div>
-        );
-      case "pizza-toppings":
-        return (
-          <div>
-            <p>
-              *These toppings are not included automatically but are available
-              by request at no additional charge.
-            </p>
-            <br />
-            <p>Like Supreme Pizza? Try our &quot;Borderline 9&quot;</p>
-          </div>
-        );
-    }
-  }
+  //           <p>
+  //             *includes canadian bacon, pepperoni, hamburger, sausage and bacon
+  //           </p>
+  //           <p>**includes onion and green pepper</p>
+  //         </div>
+  //       );
+  //     case "pizza-toppings":
+  //       return (
+  //         <div>
+  //           <p>
+  //             *These toppings are not included automatically but are available
+  //             by request at no additional charge.
+  //           </p>
+  //           <br />
+  //           <p>Like Supreme Pizza? Try our &quot;Borderline 9&quot;</p>
+  //         </div>
+  //       );
+  //   }
+  // }
 };
 
